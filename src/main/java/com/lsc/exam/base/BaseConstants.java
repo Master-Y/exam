0387f98f0a67f2ee863aa23c4c711c7db1b698fd
Master-Y <617639941@qq.com>
2017-04-30 14:15:57 +0800

    提交项目

---
+++ src/main/java/com/lsc/exam/base/BaseConstants.java
@@ -0,0 +1,10 @@
+package com.lsc.exam.base;
+
+/**
+ * 全局常量
+ * Created by shuzheng on 2017/2/18.
+ */
+public class BaseConstants {
+    public static final String SESSION_KEY_OBJ_USER_BEAN = "user_session";
+
+}
